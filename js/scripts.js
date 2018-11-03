12e778e1aa255a41f292803249cea90b8520e63c
Pascal Weber <pascal.weber@prodyna.com>
2018-11-03 20:13:45 +0100

    init

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+$('a[href*=\\#]').on('click', function(e){
+    var href = $(this).attr('href');
+    $('html, body').animate({
+        scrollTop:$(href).offset().top
+    },'slow');
+    e.preventDefault();
+});
